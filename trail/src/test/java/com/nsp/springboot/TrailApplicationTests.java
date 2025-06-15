9d526ede2ff79121c8f87b6cb4a3f4a0384e0d71
nasir <nasir@DESKTOP-HOMA18J>
2025-06-15 11:54:25 +0530

    Hi

---
+++ trail/src/test/java/com/nsp/springboot/TrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsp.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
